b00550a8ef751ad91c86a6f0aa86ecb33378b3a7
kkscwcd <kkscwcd@gmail.com>
2024-11-12 11:23:49 +0530

    first commit

---
+++ src/main/java/com/cleverthis/interview/Solution.java
@@ -0,0 +1,13 @@
+package com.cleverthis.interview;
+
+import com.cleverthis.interview.padlock.PadlockImpl;
+
+/**
+ * This is a placeholder class showing a simple boilerplate.
+ * This class is not required, so you can replace with your own architecture.
+ */
+public class Solution {
+    public void solve(PadlockImpl padlock) {
+        throw new RuntimeException("TODO");
+    }
+}
